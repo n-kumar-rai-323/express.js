d5c0fe667118a34f46ebe1f074459e1118ffb847
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2025-01-01 14:10:47 +0545

    RestAPI

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  return res.send("Hello form home page ");
+  return res.send("My Name is  " + req.query.name);
 });
 
 app.get("/about", (req, res) => {
   return res.send("Hello from About page");
 });
-const myServer = http.createServer(app);
-
-myServer.listen(8000, () => console.log("Server Started!"));
+// const myServer = http.createServer(app);
+app.listen(8000, () => console.log("Server Started!") )
+// myServer.listen(8000,);
